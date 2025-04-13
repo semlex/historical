700e7c2c183362a6bc6d25d73c4929b47dc3bac9
semlex <picasso.semenov.spb@gmail.com>
2025-04-13 03:08:16 +0300

    add block-title

---
+++ src/shared/ui/block-title/block-title.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from 'react'
+import styles from './block-title.module.scss'
+
+type BlockTitleProps = {
+  children: ReactNode
+}
+
+const BlockTitle = ({ children }: BlockTitleProps) => {
+  return <h1 className={styles.blockTitle}>{children}</h1>
+}
+
+export default BlockTitle
---
+++ src/shared/ui/index.ts
@@ -0,0 +1,1 @@
+export { default as BlockTitle } from './block-title/block-title';
