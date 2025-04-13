84cc686ba3c9d5b1c04b53063518cfe103510cbe
semlex <picasso.semenov.spb@gmail.com>
2025-04-13 03:03:41 +0300

    app refactor

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { HistoricalBlock } from 'widgets/historical-block';
+
+function App() {
+  return (
+    <div className="App">
+      <HistoricalBlock />
+      {/* <HistoricalBlock /> */}
+    </div>
+  );
+}
+
+export default App;
