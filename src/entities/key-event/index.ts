bd0fcae6fb68854e9655fe25909feb9e82a25971
semlex <picasso.semenov.spb@gmail.com>
2025-04-14 04:40:33 +0300

    export key event type

--- src/entities/key-event/index.ts
+++ src/entities/key-event/index.ts
@@ -1,3 +1,4 @@
+export type { KeyEvent as KeyEventType } from './model/types';
 export { default as KeyEvent } from './ui/key-event/key-event';
 export { default as KeyEventsSlider } from './ui/key-events-slider/key-events-slider';
 export { events } from './model/data';
