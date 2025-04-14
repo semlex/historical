a02026189d81385c5688bb01a12d04bf69a24cb8
semlex <picasso.semenov.spb@gmail.com>
2025-04-14 19:35:02 +0300

    delete unused useEffect

--- src/widgets/historical-block/ui/historical-block.tsx
+++ src/widgets/historical-block/ui/historical-block.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef } from 'react';
 import { Interval, intervals, IntervalTitle } from 'entities/interval';
 import { events, KeyEventsSlider } from 'entities/key-event';
 import { BlockTitle } from 'shared/ui';
